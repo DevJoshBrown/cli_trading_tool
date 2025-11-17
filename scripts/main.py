1a0e79218c180b87b40950943c939b547c9ff58c
DevJoshBrown <devjoshbrown@gmail.com>
2025-11-17 10:25:58 +0000

    login system functional, no passwords

--- scripts/main.py
+++ scripts/main.py
@@ -4,12 +4,7 @@ from login_funcs import login_process
 from user_database import load_user_database
 
 
-def main():
-    print("WELCOME TO THE CLI TRADING TOOL.")
-    print("loading users...")
-    load_user_database()
-    print("loading complete.")
-
+def log_in():
     login_failed = True
     while login_failed:
         result, account = login_process()
@@ -18,11 +13,27 @@ def main():
             current_user = account
             if current_user is not None:
                 print(f"hi {current_user['name']}, login successful!")
+                return current_user
+
             else:
                 login_failed = True
                 continue
         else:
             continue
+
+
+def main():
+    print("WELCOME TO THE CLI TRADING TOOL.")
+    print("loading users...")
+    load_user_database()
+    print("loading complete.")
+    current_user = log_in()
+
+    # MAIN MENU
+    while current_user is not None:
+        print(f"logged in as {current_user['name']}")
+        # main menu
+        return
 
 
 if __name__ == "__main__":
